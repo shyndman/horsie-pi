2418562f9671c7965e1844e4b23abec4d614963a
Scott Hyndman <scotty.hyndman@gmail.com>
2023-10-24 03:35:51 -0400

    Add UART for non-debug host communication

--- scratch/examples/hello_tinys3.rs
+++ scratch/examples/hello_tinys3.rs
@@ -5,10 +5,9 @@
 extern crate alloc;
 use core::mem::MaybeUninit;
 
-use defmt::Debug2Format;
 use defmt_macros::unwrap;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Instant, Timer};
 use esp32s3_hal::{
     self,
     clock::ClockControl,
@@ -17,10 +16,11 @@ use esp32s3_hal::{
     peripherals::Peripherals,
     prelude::*,
     system::SystemParts,
-    Rmt, IO,
+    uart, Rmt, Uart, IO,
 };
 use esp_hal_procmacros::main;
 use esp_hal_smartled::{smartLedAdapter, SmartLedsAdapter};
+use esp_println::logger::init_logger_from_env;
 use scratch as _;
 use smart_leds::brightness;
 use smart_leds_trait::*;
@@ -43,6 +43,20 @@ async fn run_color_wheel(
     mut power_pin: AnyPin<Output<PushPull>>,
     data_pin: AnyPin<Output<PushPull>>,
 ) {
+    fn rgb_color_wheel(mut i: u8) -> RGB8 {
+        i %= 255;
+
+        if i < 85 {
+            RGB8::new(255 - i * 3, 0, i * 3)
+        } else if i < 170 {
+            i -= 85;
+            RGB8::new(0, i * 3, 255 - i * 3)
+        } else {
+            i -= 170;
+            RGB8::new(i * 3, 255 - i * 3, 0)
+        }
+    }
+
     unwrap!(power_pin.set_high());
     let data_pin: Gpio18<Output<PushPull>> = data_pin.try_into().unwrap();
 
@@ -51,7 +65,6 @@ async fn run_color_wheel(
     let mut color_index = 0u8;
     loop {
         let color = rgb_color_wheel(color_index);
-        defmt::debug!("{}: {}", color_index, Debug2Format(&color));
         unwrap!(led.write(brightness(core::iter::once(color), 90)));
 
         color_index = color_index.wrapping_add(1);
@@ -59,23 +72,23 @@ async fn run_color_wheel(
     }
 }
 
-fn rgb_color_wheel(mut i: u8) -> RGB8 {
-    i %= 255;
-
-    if i < 85 {
-        RGB8::new(255 - i * 3, 0, i * 3)
-    } else if i < 170 {
-        i -= 85;
-        RGB8::new(0, i * 3, 255 - i * 3)
-    } else {
-        i -= 170;
-        RGB8::new(i * 3, 255 - i * 3, 0)
+#[embassy_executor::task]
+async fn run_host_communication(mut uart: Uart<'static, esp32s3_hal::peripherals::UART1>) {
+    loop {
+        match embedded_io_async::Write::write_all(&mut uart, "hello hello\n".as_bytes()).await
+        {
+            Ok(_) => defmt::debug!("uart1 write successful"),
+            Err(e) => defmt::error!("uart1 write failure, {}", e),
+        }
+        Timer::after(Duration::from_secs(2)).await;
     }
 }
 
 #[main]
 async fn main(spawner: Spawner) {
     init_heap();
+    init_logger_from_env();
+
     defmt::info!("Init!");
 
     let peripherals = Peripherals::take();
@@ -84,6 +97,15 @@ async fn main(spawner: Spawner) {
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
+    let uart1 = Uart::new_with_config(
+        peripherals.UART1,
+        uart::config::Config {
+            baudrate: 115_200,
+            ..uart::config::Config::default()
+        },
+        Some(uart::TxRxPins::new_tx_rx(io.pins.gpio7, io.pins.gpio6)),
+        &clocks,
+    );
     embassy::init(
         &clocks,
         esp32s3_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
@@ -97,4 +119,13 @@ async fn main(spawner: Spawner) {
         neopixel_power_pin.degrade(),
         neopixel_data_pin.degrade(),
     )));
+
+    unwrap!(spawner.spawn(run_host_communication(uart1)));
+
+    loop {
+        let uptime_seconds = Instant::now().as_millis() as f32 / 1000.0;
+
+        defmt::debug!("Up {} seconds", uptime_seconds);
+        Timer::after(Duration::from_secs(10)).await;
+    }
 }
