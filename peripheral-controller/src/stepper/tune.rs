ed4346b094ec992bb481bdb84dad7ceb8445bb72
Scott Hyndman <scotty.hyndman@gmail.com>
2023-11-03 02:34:02 -0400

    Update stepper tuning to use SpreadCycle

--- peripheral-controller/src/stepper/tune.rs
+++ peripheral-controller/src/stepper/tune.rs
@@ -1,9 +1,23 @@
 use embassy_sync::blocking_mutex::raw::RawMutex;
+use micromath::F32Ext;
 
 use super::{
     motor_constants::{MotorConstants, TMC2209_VSENSE_OHMS},
     uart::Tmc2209UartConnection,
 };
+
+pub enum TuningProfile {
+    Silent,
+    Performance,
+}
+
+const IHOLD_DELAY: u8 = 12;
+
+const SEMIN: u16 = 8;
+const SEMAX: u16 = 4;
+const SEUP: u16 = 3;
+const SEDN: u16 = 0;
+const SEIMIN: bool = false; // If we drop to 1/4 current, high accels don't work right
 
 pub async fn tune_driver<M: RawMutex, P: esp_hal_common::uart::Instance>(
     driver: &mut Tmc2209UartConnection<M, P>,
@@ -23,43 +37,50 @@ pub async fn tune_driver<M: RawMutex, P: esp_hal_common::uart::Instance>(
     defmt::info!("Setting IRUN={}, IHOLD={}", irun, ihold);
     ihold_irun.set_irun(irun);
     ihold_irun.set_ihold(ihold);
+    ihold_irun.set_ihold_delay(IHOLD_DELAY);
     driver.write_register(ihold_irun).await.unwrap();
 
+    let mut tpowerdown = tmc2209::reg::TPOWERDOWN::default();
+    tpowerdown.0 = 20;
+    driver.write_register(tpowerdown).await.unwrap();
+
     let mut chopconf = tmc2209::reg::CHOPCONF::default();
-    let (hstrt, hend) = motor_constants.hysteresis(None, Some(12.0), None, None);
+    let (hstrt, hend) = motor_constants.hysteresis(None, None, None, None);
     chopconf.set_hstrt(hstrt);
     chopconf.set_hstrt(hend);
     chopconf.set_mres(0b00); // 256 steps
     chopconf.set_intpol(true); // Interpolation of microsteps to 256
     chopconf.set_vsense(vsense);
+    chopconf.set_tbl(1);
+    chopconf.set_toff(((0.85e-5 * tmc2209::INTERNAL_CLOCK_HZ - 12.0) / 32.0).ceil() as u32);
+    chopconf.set_dedge(true);
     driver.write_register(chopconf).await.unwrap();
-
-    // StealthChop configuration
-    let mut pwmconf = tmc2209::reg::PWMCONF::default();
-    pwmconf.set_pwm_autoscale(true);
-    pwmconf.set_pwm_autograd(true);
-    pwmconf.set_pwm_grad(motor_constants.pwm_gradient(None, None) as u8);
-    pwmconf.set_pwm_ofs(motor_constants.pwm_output_frequency(None) as u8);
-    pwmconf.set_pwm_reg(15);
-    pwmconf.set_pwm_lim(4);
-    driver.write_register(pwmconf).await.unwrap();
 
     // Stallguard
     let mut tpwnthrs = tmc2209::reg::TPWMTHRS::default();
-    tpwnthrs.set(0); // Disable switching between StealthChop and SpreadCycle
+    tpwnthrs.set(0xfffff); // This keeps the stepper in SpreadCycle mode
     driver.write_register(tpwnthrs).await.unwrap();
 
     // Coolstep
     let mut tcoolthrs = tmc2209::reg::TCOOLTHRS::default();
-    tcoolthrs.set(56);
+    tcoolthrs.set(293);
     driver.write_register(tcoolthrs).await.unwrap();
 
     let mut sgthrs = tmc2209::reg::SGTHRS::default();
-    sgthrs.0 = 1;
+    sgthrs.0 = 10;
     driver.write_register(sgthrs).await.unwrap();
 
-    gconf.set_en_spread_cycle(false); // StealthChop only
+    let mut coolconf = tmc2209::reg::COOLCONF::default();
+    coolconf.set_semin(SEMIN);
+    coolconf.set_semax(SEMAX);
+    coolconf.set_seup(SEUP);
+    coolconf.set_sedn(SEDN);
+    coolconf.set_seimin(SEIMIN);
+    driver.write_register(coolconf).await.unwrap();
+
+    gconf.set_en_spread_cycle(true); // SpreadCycle
+    gconf.set_i_scale_analog(false);
+    gconf.set_multistep_filt(true);
     gconf.set_mstep_reg_select(true); // Set microsteps via registers
-    gconf.set_pdn_disable(false); // Re-enable PDN
     driver.write_register(gconf).await.unwrap();
 }
